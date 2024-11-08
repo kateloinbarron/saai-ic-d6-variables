0c9335dfc5e41da0daef3281cf8f2e86edc4f00e
Kate Loin-Barron <kateloin06@gmail.com>
2024-11-08 04:14:07 +0000

    JS code first time

---
+++ script.js
@@ -0,0 +1,29 @@
+let animal = "dog"
+console.log ("I love writing JS code")
+console.log ("I'm a second line of JS code")
+console.log (animal)
+console.log ("I'm still here...")
+//string 
+let personName = "Kate"
+console.log ("personName")
+
+//Number
+let purchasePrice = 14.99
+console.log("initial value purchasePrice", purchasePrice)
+
+//Boolean
+let isTuesday = false
+console.log (isTuesday)
+
+//Try It Yourself
+// Declare and console.log 3 variables of your choice
+// Data types: string, number, boolean
+
+
+
+
+
+
+
+
+
